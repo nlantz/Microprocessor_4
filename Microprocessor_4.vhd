e8fd125c7647ba4d1254c398b140ed4f15b9af8a
nlantz <nicholaslantz@gmail.com>
2014-11-19 17:13:07 -0700

    working commit

--- Microprocessor_4.vhd
+++ Microprocessor_4.vhd
@@ -3,6 +3,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity Microprocessor_4 is
     Port ( CLK : in STD_LOGIC;
+			  MANUAL_CLK : in STD_LOGIC;
 			  nAONLY : in  STD_LOGIC;
            INVERT : in  STD_LOGIC;
            LOGIC : in  STD_LOGIC;
@@ -14,7 +15,9 @@ entity Microprocessor_4 is
            BUFFER_IN : in  STD_LOGIC_VECTOR (3 downto 0);
            A_OUT : out  STD_LOGIC_VECTOR (3 downto 0);
 			  DEC_IN : in  STD_LOGIC_VECTOR (3 downto 0);
-			  REG_CLR : in STD_LOGIC);
+			  REG_CLR : in STD_LOGIC;
+			  AN: in std_logic_vector(3 downto 0);								-- 4-bit anode controller
+		     SSEG: in std_logic_vector(6 downto 0));
 end Microprocessor_4;
 
 architecture Behavioral of Microprocessor_4 is
@@ -45,9 +48,18 @@ end COMPONENT;
 COMPONENT Register_4bit is
     Port ( Input : in  STD_LOGIC_VECTOR (3 downto 0);
            EN : in  STD_LOGIC;
-           clk : in  STD_LOGIC;
+           CLK : in  STD_LOGIC;
            clear : in  STD_LOGIC;	-- This is active high
            Output : out  STD_LOGIC_VECTOR (3 downto 0));
+end COMPONENT;
+
+COMPONENT display_controller is
+	port(
+		CLK, RESET: in std_logic;												-- 100MHz clock and RESET button (single bit)
+		HEX3, HEX2, HEX1, HEX0: in std_logic_vector(3 downto 0);		-- 4-bit inputs for the LED lights
+		AN: out std_logic_vector(3 downto 0);								-- 4-bit anode controller
+		SSEG: out std_logic_vector(6 downto 0)								-- 7-bit cathode controller
+		);
 end COMPONENT;
 
 signal A, B, ALU_out, REG_4_2_out : STD_LOGIC_VECTOR(3 downto 0); 
@@ -61,14 +73,16 @@ REG_and <= DEC_out(0) and DEC_out(15);
 
 ALU_1 : ALU port map(A, B, ALU_out, '0',  ALU_cout, LOGIC, INVERT, nAONLY);
 
-REG_4_1 : Register_4bit port map(ALU_out, ACCUMULATORIE, CLK, CLEAR, B);
-REG_4_2 : Register_4bit port map(A, REG_and, CLK, REG_CLR, REG_4_2_out);
+REG_4_1 : Register_4bit port map(ALU_out, ACCUMULATORIE, MANUAL_CLK, CLEAR, B);
+REG_4_2 : Register_4bit port map(A, REG_and, MANUAL_CLK, REG_CLR, REG_4_2_out);
 
 BUF_4_1 : Buffer_4bit port map( B, ACCUMULATOROE, ACCUMULATOROE, A);
 BUF_4_2 : Buffer_4bit port map( REG_4_2_out, READ1, DEC_OUT(15), A);
 BUF_4_3 : Buffer_4bit port map( BUFFER_IN, DEC_OUT(0), READ1, A);
 
 DEC_4_1 : DEC_4 port map('1', DEC_IN, DEC_out);
+
+DISP_CONT : display_controller port map( CLK, CLEAR, A, B, REG_4_2_out, "0000", AN, SSEG);
 
 end Behavioral;
 
